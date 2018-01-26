dd669f474d7cb409ab513acb63bc3110aec2657c
yapengren <404210666@qq.com>
2018-01-27 02:24:52 +0800

    Delete some files

--- maven_parent/maven_dao/src/main/java/com/yapengren/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.yapengren.dao;
-
-import com.yapengren.domain.Customer;
-
-public interface CustomerDao {
-
-	Customer findById(int custId);
-
-}
--- maven_parent/maven_service/src/main/java/com/yapengren/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.yapengren.service;
-
-import com.yapengren.domain.Customer;
-
-public interface CustomerService {
-
-	Customer findById(int custId);
-
-}
