96c2a44a57f28ffa01fbed00e4a39f6810624566
yapengren <404210666@qq.com>
2018-01-28 14:27:50 +0800

    用户列表&添加

--- bos_management_parent/bos_management_service/src/main/java/com/yapengren/bos/service/system/impl/UserServiceImpl.java
+++ bos_management_parent/bos_management_service/src/main/java/com/yapengren/bos/service/system/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.yapengren.bos.service.system.impl;
 
+import com.yapengren.bos.dao.system.RoleDao;
 import com.yapengren.bos.dao.system.UserDao;
+import com.yapengren.bos.domain.system.Role;
 import com.yapengren.bos.domain.system.User;
 import com.yapengren.bos.service.system.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao ud;
 
+    @Autowired
+    private RoleDao rd;
+
     /**
      * 查询用户列表
      * @return
@@ -23,5 +28,20 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return ud.findAll();
+    }
+
+    /**
+     * 保存用户
+     * @param model
+     * @param roleIds
+     */
+    @Override
+    public void save(User model, List<Integer> roleIds) {
+        //查询关联的角色
+        List<Role> roles = rd.findAll(roleIds);
+        //表达关系
+        model.getRoles().addAll(roles);
+        //保存用户
+        ud.save(model);
     }
 }
